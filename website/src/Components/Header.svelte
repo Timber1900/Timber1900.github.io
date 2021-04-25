aa273b7adf028f5af5db4223b7fb5ad47c917800
Timber1900 <timber.gamer@gmail.com>
2021-04-26 00:37:42 +0100

    Header icon names

--- website/src/Components/Header.svelte
+++ website/src/Components/Header.svelte
@@ -2,19 +2,33 @@
   import GithubLogo from "./GithubLogo.svelte";
   import DiscordLogo from "./DiscordLogo.svelte";
   import SteamLogo from "./SteamLogo.svelte";
+  import YoutubeLogo from "./YoutubeLogo.svelte";
+  import TwitchLogo from "./TwitchLogo.svelte";
+
 </script>
 
 <header class="text-primary-color dark-primary-color">
   <h1>Timber's Github Projects</h1>
   <div class="icons-outer">
-    <div>
+    <div class="svg-div">
       <GithubLogo />
+      <p>Github</p>
     </div>
-    <div>
+    <div class="svg-div">
       <DiscordLogo />
+      <p>Discord</p>
     </div>
-    <div>
+    <div class="svg-div">
       <SteamLogo />
+      <p>Steam</p>
+    </div>
+    <div class="svg-div">
+      <YoutubeLogo />
+      <p>Youtube</p>
+    </div>
+    <div class="svg-div">
+      <TwitchLogo />
+      <p>Twitch</p>
     </div>
   </div>
 </header>
@@ -38,5 +52,29 @@
 
   .icons-outer > div {
     padding: 0 1em;
+  }
+
+  .svg-div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    white-space: nowrap;
+  }
+
+  .svg-div > p {
+    margin: 0;
+    padding: 0 0 0 0;
+    width: 0;
+    overflow: hidden;
+    transition: all ease 0.5s;
+    border-radius: 2ch;
+    color: #eeeeee00;
+  }
+
+  .svg-div:hover > p {
+    padding: 0 0 0 2ch;
+    width: 8ch;
+    color: #eeeeeeff;
   }
 </style>
---
+++ website/src/Components/YoutubeLogo.svelte
@@ -0,0 +1,23 @@
+<svg width="48px" height="48px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 192 192" style="enable-background:new 0 0 192 192;" xml:space="preserve">
+<style type="text/css">
+	.st1{fill:#222831}
+</style>
+<g>
+	<path d="M180.3,53.4c-2-7.6-8-13.6-15.6-15.7C151,34,96,34,96,34s-55,0-68.8,3.7c-7.6,2-13.5,8-15.6,15.7
+		C8,67.2,8,96,8,96s0,28.8,3.7,42.6c2,7.6,8,13.6,15.6,15.7C41,158,96,158,96,158s55,0,68.8-3.7c7.6-2,13.5-8,15.6-15.7
+		C184,124.8,184,96,184,96S184,67.2,180.3,53.4z"/>
+	<polygon class="st1" points="78,122.2 78,69.8 124,96 	"/>
+</g>
+</svg>
+
+<style>
+  svg {
+    fill: #607D8B;
+    transition: fill ease 0.25s;
+  }
+
+  svg:hover {
+    fill: #00adb5;
+  }
+</style>
