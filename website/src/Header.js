b91df55026c72cac243e5a330f9d5c64f87af628
Timber1900 <timber.gamer@gmail.com>
2021-06-17 19:06:52 +0100

    removed top sticky on small height devices

--- website/src/Header.js
+++ website/src/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 
 
   return(
-    <header className="bg-white dark:bg-gray-800 w-full flex justify-center sticky top-0">
+    <header className="bg-white dark:bg-gray-800 w-full flex justify-center custombp:sticky custombp:top-0">
       <div className="flex h-20 place-items-center my-2 border-b border-gray-200 dark:border-gray-700 w-full-minus-padding relative">
         <h1 className="text-center text-xl sm:text-4xl font-sans font-bold mx-auto text-black dark:text-white">Timber's Github Projects</h1>
         <label htmlFor='switch'>
